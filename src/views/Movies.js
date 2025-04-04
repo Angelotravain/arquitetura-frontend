8af9753a1189ca2fff4acf04f3ac97f298260bc9
Angelotravain <angelotravain96@gmail.com>
2025-04-03 21:42:06 -0300

    Criação de hooks, ajuste an estrutura de pastas, inclusão de novo componente, uso de css global e por arquivo, ajustes da interfaçe anterior, finalizado até a aula 3.

--- src/views/Movies.js
+++ src/views/Movies.js
@@ -1,3 +1,15 @@
+import { useMovies } from './../hooks/useMovies';
+import styles from './movies.module.css';
+import { MovieItem } from './../components/MovieItem/MovieItem';
+
 export function Movies(){
-    return <h1>Movies page</h1>
+    const movies = useMovies();
+    return (
+        <section className={styles.moviesContainer}>
+            <h1>Filmes populares</h1>
+            <div className={styles.moviesList}>
+                {movies.map((movie) =>  <MovieItem key={movie.id} movie={movie}></MovieItem>)}
+            </div>
+        </section>
+    );
 }
